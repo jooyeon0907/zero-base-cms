66724f730118ce3675b07454846635db7e22b796
jooyeon <jooyeon0907@naver.com>
2025-01-12 14:45:21 +0900

    feat : mailgun secret key 노출 안되도록 변경

--- user-api/src/main/java/com/zerobase/cms/user/config/FeignConfig.java
+++ user-api/src/main/java/com/zerobase/cms/user/config/FeignConfig.java
@@ -2,20 +2,19 @@ package com.zerobase.cms.user.config;
 
 import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfig {
 
+	@Value(value = "${mailgun.key}")
+	private String mailgunPKey;
+
 	@Qualifier(value = "mailgun")
 	@Bean
 	public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
-		return new BasicAuthRequestInterceptor("api", "e03562a87eceaa2ede66c88377c04821-7113c52e-3d5809ff");
+		return new BasicAuthRequestInterceptor("api", mailgunPKey);
 	}
 }
-// privateKey
-// 2dfc8ca9e322d9681fe33a1a29f0f88b-7113c52e-c23f1657
-
-// tetmail
-// e03562a87eceaa2ede66c88377c04821-7113c52e-3d5809ff
